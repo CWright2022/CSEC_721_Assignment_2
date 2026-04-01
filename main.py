722a013c52bacded074f93ecc4dc480c3e4b0dde
Cayden Wright <cwright2022@gmail.com>
2026-04-01 12:47:18 -0400

    add timing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # CSEC721 Homework/Lab 2: Homomorphic Encryption
 # Cayden Wright, Spring 2026
 import tenseal as ts
+import time
 import csv
 
 NUM_RECORDS = 10
@@ -9,13 +10,16 @@ NUM_RECORDS = 10
 def getPrediction(enc_values):
     # This runs on the server - it performs these values without decrypting them
     # 0.5581×age + 0.0048×trestbps + 0.0044×chol - 0.0036×thalach + 0.1290×oldpeak - 28.9796
+    pre_prediction_time = time.time()
     result = (0.5581 * enc_values[0] + 
               0.0048 * enc_values[3] + 
               0.0044 * enc_values[4] - 
               0.0036 * enc_values[7] + 
               0.1290 * enc_values[9] - 
               28.9796)
-    return result
+    post_prediction_time = time.time()
+    duration = post_prediction_time - pre_prediction_time
+    return result, duration
 
 
 def main():
@@ -33,13 +37,24 @@ def main():
                          coeff_mod_bit_sizes=[60, 40, 40, 60])
             context.generate_galois_keys()
             context.global_scale = 2**40
-            # Encrypt
+            
+            # Encrypt (timing this operation)
+            pre_enc_time = time.time()
             enc_values = [ts.ckks_vector(context, [float(val)]) for val in record]
+            post_enc_time = time.time()
+            enc_duration = post_enc_time - pre_enc_time
+            
             # this "sends the values to the server" - server does not know true values
-            prediction = getPrediction(enc_values)
-            # Decrypt
+            prediction, pred_duration = getPrediction(enc_values)
+            
+            # Decrypt (timing this too)
+            pre_dec_time = time.time()
             dec_prediction = prediction.decrypt()[0]
-            print(f"Record {i}: Prediction = {dec_prediction}")
+            post_dec_time = time.time()
+            dec_duration = post_dec_time - pre_dec_time
+            binary_prediction = "HIGH RISK" if dec_prediction >0 else "LOW RISK"
+            
+            print(f"Record {i}: Prediction = {dec_prediction} ({binary_prediction}) - Encryption: {enc_duration:.4f} Prediction: {pred_duration:.4f} Decryption: {dec_duration:.4f}")
             i += 1
 
 if __name__ == "__main__":
